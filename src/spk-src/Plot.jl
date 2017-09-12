2f41196f12a90b31d439de2c24ada38783dd5ca8
Scottie Alexander <scottiealexander11@gmail.com>
2017-09-12 10:51:37 -0700

    Add raster and cycle mean plotting

--- src/spk-src/Plot.jl
+++ src/spk-src/Plot.jl
@@ -35,3 +35,33 @@ function plot_xcorr{T<:AbstractFloat}(ts1::Vector{T}; bin_size=0.0005, tmax=0.01
     return h
 end
 # ============================================================================ #
+function plot_raster{T<:AbstractFloat}(ts::Vector{T}, evt::AbstractArray{T,1}, dur::AbstractFloat, color::String="blue")
+    const bin_size = 0.001
+    nbin = floor(Int, dur / bin_size)
+    p = psth(ts, collect(evt), 0:nbin, bin_size)[1];
+    h = default_figure()
+    for k = 1:size(p,2)
+       idx = find(p[:,k])
+       plot(idx * bin_size, fill(k, length(idx)), ".", color=color)
+   end
+   xlabel("Time (sec)", fontsize=16)
+   ylabel("Trial #", fontsize=16)
+   return h
+end
+# ============================================================================ #
+function plot_cycle_mean{T<:AbstractFloat}(ts::Vector{T},
+    evt::AbstractArray{T,1}, dur::AbstractFloat, tf::AbstractFloat,
+    color::String="blue")
+
+    const bin_size = 0.001
+    nbin = floor(Int, dur / bin_size)
+    bpc = floor(Int, 1/(tf * bin_size))
+    p = psth(ts, collect(evt), 0:nbin, bin_size)[1];
+    h = default_figure()
+    cm = cycle_mean(sum(p, 2), bpc)
+    h = plot(linspace(0, 1/tf, length(cm)), cm, color=color)
+    xlabel("Time (sec)", fontsize=16)
+    ylabel("Firing rate (Hz)", fontsize=16)
+    return h
+end
+# ============================================================================ #
